79a905b4e93d32ee840be4890555cb6771473dec
“muhammaddaniyal947” <daniyalcsonly40@gmail.com>
2024-04-18 17:49:18 +0500

    ok

---
+++ app.js
@@ -0,0 +1,62 @@
+// ------- CHAPTER 43 - 49 ---------- //
+
+// Q1 
+
+// function myAlert(){
+//     alert('ALERT WORKING')
+// }
+
+
+// var onclick = ()=>{
+//     alert('thank you for purchase')
+// }
+
+
+
+// var counterElement = document.getElementById('counter');
+// var increaseBtn = document.getElementById('increaseBtn');
+// var decreaseBtn = document.getElementById('decreaseBtn');
+
+
+// let count = 0; 
+
+// function updateCounter() {
+//   counterElement.textContent = count;
+// }
+
+// increaseBtn.addEventListener('click', function() {
+//   count++;
+//   updateCounter();
+// });
+
+// decreaseBtn.addEventListener('click', function() {
+//   count--;
+//   updateCounter();
+// });
+
+
+// ------- CHAPTER 49-52 ------------- //
+
+    // Q1
+    // function displayData() {
+    //     event.preventDefault(); 
+        
+       
+    //     var name = document.getElementById("name").value;
+    //     var email = document.getElementById("email").value;
+        
+    
+    //     var formDataDisplay = document.getElementById("formData");
+    //     formDataDisplay.innerHTML = "<strong>Name:</strong> " + name + "<br><strong>Email:</strong> " + email;
+        
+    //     document.getElementById("signupForm").reset();
+        
+    //     return false;
+    // }
+
+    // // Add event listener to the form for form submission
+    // document.getElementById("signupForm").addEventListener("submit", displayData);
+
+
+
+    //--------------XXX END ----------------- // XX 
